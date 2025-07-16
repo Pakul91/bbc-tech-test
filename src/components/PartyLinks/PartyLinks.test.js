1ec0d853a4680745fe9373e1180e47badeb14b04
Tomasz Pakulniewicz <pakulniewicz.t@gmail.com>
2025-07-16 16:21:44 +0100

    party links formatted

--- src/components/PartyLinks/PartyLinks.test.js
+++ src/components/PartyLinks/PartyLinks.test.js
@@ -1,24 +1,26 @@
-import { render } from '@testing-library/react';
-import PartyLinks from '.';
+import { render, screen } from "@testing-library/react";
+import PartyLinks from ".";
 
-test('renders party links as a list', async () => {
-  const { container } = render(<PartyLinks/>);
+const PARTIES = [
+  { name: "Hippo Party", url: "https://en.wikipedia.org/wiki/Hippopotamus" },
+  { name: "Giraffe Party", url: "https://en.wikipedia.org/wiki/Giraffe" },
+  { name: "Tiger Party", url: "https://en.wikipedia.org/wiki/Tiger" },
+  { name: "Zebra Party", url: "https://en.wikipedia.org/wiki/Zebra" },
+  { name: "Meerkat Party", url: "https://en.wikipedia.org/wiki/Meerkat" },
+  { name: "Pelican Party", url: "https://en.wikipedia.org/wiki/Pelican" },
+];
 
-  const partyLinks = container.querySelector('.Party-links')
-  expect(partyLinks.nodeName).toBe('UL');
+test("renders all the party links", async () => {
+  render(<PartyLinks />);
+  const partyLinks = screen.getAllByRole("link");
+  expect(partyLinks.length).toBe(6);
 });
 
-test('renders all the party links', async () => {
-  const { container } = render(<PartyLinks/>);
+test("renders a party link with the correct text and href", async () => {
+  render(<PartyLinks />);
 
-  const partyLink = container.querySelectorAll('li.Party-link')
-  expect(partyLink.length).toBe(6);
-});
-
-test('renders a party link with the correct text and href', async () => {
-  const { container } = render(<PartyLinks/>);
-
-  const partyLinksAnchorTag = container.querySelector('.Party-link a')
-  expect(partyLinksAnchorTag.textContent).toBe('Hippo Party');
-  expect(partyLinksAnchorTag.href).toBe('https://en.wikipedia.org/wiki/Hippopotamus');
+  PARTIES.forEach((party) => {
+    const partyLink = screen.getByRole("link", { name: party.name });
+    expect(partyLink).toHaveAttribute("href", party.url);
+  });
 });
--- src/components/PartyLinks/index.js
+++ src/components/PartyLinks/index.js
@@ -1,23 +1,25 @@
-import './PartyLinks.css';
+import "./PartyLinks.css";
 
 const PARTIES = [
-  { name: 'Hippo Party', url: 'https://en.wikipedia.org/wiki/Hippopotamus'},
-  { name: 'Giraffe Party', url: 'https://en.wikipedia.org/wiki/Giraffe'},
-  { name: 'Tiger Party', url: 'https://en.wikipedia.org/wiki/Tiger'},
-  { name: 'Zebra Party', url: 'https://en.wikipedia.org/wiki/Zebra'},
-  { name: 'Meerkat Party', url: 'https://en.wikipedia.org/wiki/Meerkat'},
-  { name: 'Pelican Party', url: 'https://en.wikipedia.org/wiki/Pelican'},
-]
+  { name: "Hippo Party", url: "https://en.wikipedia.org/wiki/Hippopotamus" },
+  { name: "Giraffe Party", url: "https://en.wikipedia.org/wiki/Giraffe" },
+  { name: "Tiger Party", url: "https://en.wikipedia.org/wiki/Tiger" },
+  { name: "Zebra Party", url: "https://en.wikipedia.org/wiki/Zebra" },
+  { name: "Meerkat Party", url: "https://en.wikipedia.org/wiki/Meerkat" },
+  { name: "Pelican Party", url: "https://en.wikipedia.org/wiki/Pelican" },
+];
 
 function PartyLinks() {
   return (
-    <ul className="Party-links">
-      {PARTIES.map((party) =>
-        (<li className="Party-link" key={party.name}>
-          <a href={party.url}>{party.name}</a>
-        </li>)
-      )}
-    </ul>
+    <div className="Party-links">
+      {PARTIES.map((party) => (
+        <span className="Party-link" key={party.name}>
+          <span className="Party-link-anchor-container">
+            <a href={party.url}>{party.name}</a>
+          </span>
+        </span>
+      ))}
+    </div>
   );
 }
 
